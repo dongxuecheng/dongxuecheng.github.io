4a09aba98f20aecc553fe2b58b529abf895464e9
Dong Xuecheng <dongxuecheng@outlook.com>
2020-02-14 15:05:14 +0800

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -55,6 +55,7 @@ function timeElapse(date){
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 时 <span class=\"digit\">" + minutes + "</span> 分 <span class=\"digit\">" + seconds + "</span> 秒"; 
+	var result = "第 <span class=\"digit\">" + "500" + "</span> 天 <span class=\"digit\">" + "00" + "</span> 时 <span class=\"digit\">" + "00" + "</span> 分 <span class=\"digit\">" + "" + "</span> 秒"; 
+	  // var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 时 <span class=\"digit\">" + minutes + "</span> 分 <span class=\"digit\">" + seconds + "</span> 秒"; 
 	$("#clock").html(result);
 }
